f7271f06143c06742e546dad9210075ac9bee335
jbreton <jbreton@kronostechnologies.com>
2018-03-06 09:21:53 -0500

    PSR 1/2 format

--- src/Regroup.php
+++ src/Regroup.php
@@ -4,64 +4,52 @@ namespace Kronos;
 
 class Regroup
 {
-	public function regroupIdenticalElements($array, callable $comparisonFunction)
-	{
-		$sets = [];
+    public function regroupIdenticalElements($array, callable $comparisonFunction)
+    {
+        $sets = [];
 
-		foreach($array as $element)
-		{
-			$matchedSet = false;
-			foreach($sets as $setIndex => $set)
-			{
-				foreach($set as $setMember) {
-					if($comparisonFunction($setMember, $element))
-					{
-						if($matchedSet === false)
-						{
-							$sets[$setIndex][] = $element;
-							$matchedSet = $setIndex;
-							break;
-						}
-						else
-						{
-							foreach($set as $mergedSetMember)
-							{
-								$sets[$matchedSet][] = $mergedSetMember;
-							}
-							unset($sets[$setIndex]);
-							break;
-						}
-					}
-				}
-			}
+        foreach ($array as $element) {
+            $matchedSet = false;
+            foreach ($sets as $setIndex => $set) {
+                foreach ($set as $setMember) {
+                    if ($comparisonFunction($setMember, $element)) {
+                        if ($matchedSet === false) {
+                            $sets[$setIndex][] = $element;
+                            $matchedSet = $setIndex;
+                            break;
+                        } else {
+                            foreach ($set as $mergedSetMember) {
+                                $sets[$matchedSet][] = $mergedSetMember;
+                            }
+                            unset($sets[$setIndex]);
+                            break;
+                        }
+                    }
+                }
+            }
 
-			if($matchedSet === false)
-			{
-				$sets[] = [$element];
-			}
-		}
+            if ($matchedSet === false) {
+                $sets[] = [$element];
+            }
+        }
 
-		return $sets;
-	}
+        return $sets;
+    }
 
-	public function regroupByHash($array, callable $hashFunction)
-	{
-		$sets = [];
+    public function regroupByHash($array, callable $hashFunction)
+    {
+        $sets = [];
 
-		foreach($array as $element)
-		{
-			$hash = $hashFunction($element);
+        foreach ($array as $element) {
+            $hash = $hashFunction($element);
 
-			if(isset($sets[$hash]))
-			{
-				$sets[$hash][] = $element;
-			}
-			else
-			{
-				$sets[$hash] = [$element];
-			}
-		}
+            if (isset($sets[$hash])) {
+                $sets[$hash][] = $element;
+            } else {
+                $sets[$hash] = [$element];
+            }
+        }
 
-		return array_values($sets);
-	}
+        return array_values($sets);
+    }
 }
--- tests/RegroupTest.php
+++ tests/RegroupTest.php
@@ -6,191 +6,188 @@ use Kronos\Regroup;
 
 class RegroupTest extends \PHPUnit_Framework_TestCase
 {
-	const FIRST_ELEMENT = 'firstElement';
-	const SECOND_ELEMENT = 'secondElement';
-	const UNIQUE_ELEMENT = 'uniqueElement';
-	const FIRST_MATCH = 'firstMatch';
-	const SECOND_MATCH = 'secondMatch';
+    const FIRST_ELEMENT = 'firstElement';
+    const SECOND_ELEMENT = 'secondElement';
+    const UNIQUE_ELEMENT = 'uniqueElement';
+    const FIRST_MATCH = 'firstMatch';
+    const SECOND_MATCH = 'secondMatch';
 
-	/**
-	 * @var Regroup
-	 */
-	private $regroup;
+    /**
+     * @var Regroup
+     */
+    private $regroup;
 
-	public function setUp()
-	{
-		$this->regroup = new Regroup();
-	}
+    public function setUp()
+    {
+        $this->regroup = new Regroup();
+    }
 
-	public function test_UnmatchedElements_regroupIdenticalElements_ShouldReturnASetForEachElements()
-	{
-		$elements = [
-			self::FIRST_ELEMENT,
-			self::SECOND_ELEMENT
-		];
+    public function test_UnmatchedElements_regroupIdenticalElements_ShouldReturnASetForEachElements()
+    {
+        $elements = [
+            self::FIRST_ELEMENT,
+            self::SECOND_ELEMENT
+        ];
 
-		$comparisonFunction = function($elmt1, $elmt2)
-		{
-			return false;
-		};
+        $comparisonFunction = function ($elmt1, $elmt2) {
+            return false;
+        };
 
-		$expectedSets = [
-			[
-				self::FIRST_ELEMENT
-			],
-			[
-				self::SECOND_ELEMENT
-			]
-		];
+        $expectedSets = [
+            [
+                self::FIRST_ELEMENT
+            ],
+            [
+                self::SECOND_ELEMENT
+            ]
+        ];
 
-		$regroupedElements = $this->regroup->regroupIdenticalElements($elements, $comparisonFunction);
+        $regroupedElements = $this->regroup->regroupIdenticalElements($elements, $comparisonFunction);
 
-		$this->assertEquals($expectedSets, $regroupedElements);
-	}
+        $this->assertEquals($expectedSets, $regroupedElements);
+    }
 
-	public function test_TwoMatchingElements_regroupIdenticalElements_ShouldReturnArrayContainingBothElemenetsInASet()
-	{
-		$elements = [
-			self::FIRST_ELEMENT,
-			self::SECOND_ELEMENT
-		];
+    public function test_TwoMatchingElements_regroupIdenticalElements_ShouldReturnArrayContainingBothElemenetsInASet()
+    {
+        $elements = [
+            self::FIRST_ELEMENT,
+            self::SECOND_ELEMENT
+        ];
 
-		$comparisonFunction = function($elmt1, $elmt2)
-		{
-			return true;
-		};
+        $comparisonFunction = function ($elmt1, $elmt2) {
+            return true;
+        };
 
-		$regroupedElements = $this->regroup->regroupIdenticalElements($elements, $comparisonFunction);
+        $regroupedElements = $this->regroup->regroupIdenticalElements($elements, $comparisonFunction);
 
-		$this->assertEquals([$elements], $regroupedElements);
-	}
+        $this->assertEquals([$elements], $regroupedElements);
+    }
 
-	public function test_MatchingElementsInArray_regroupIdenticalElements_ShouldReturnArrayContainingMatchingElementsInSets()
-	{
-		$elements = [
-			self::FIRST_MATCH,
-			self::SECOND_MATCH,
-			self::UNIQUE_ELEMENT,
-			self::FIRST_MATCH,
-			self::SECOND_MATCH
-		];
+    public function test_MatchingElementsInArray_regroupIdenticalElements_ShouldReturnArrayContainingMatchingElementsInSets(
+    )
+    {
+        $elements = [
+            self::FIRST_MATCH,
+            self::SECOND_MATCH,
+            self::UNIQUE_ELEMENT,
+            self::FIRST_MATCH,
+            self::SECOND_MATCH
+        ];
 
-		$comparisonFunction = function($elmt1, $elmt2)
-		{
-			return $elmt1 == $elmt2;
-		};
+        $comparisonFunction = function ($elmt1, $elmt2) {
+            return $elmt1 == $elmt2;
+        };
 
-		$expectedSets = [
-			[
-				self::FIRST_MATCH,
-				self::FIRST_MATCH
-			],
-			[
-				self::SECOND_MATCH,
-				self::SECOND_MATCH
-			],
-			[
-				self::UNIQUE_ELEMENT
-			]
-		];
+        $expectedSets = [
+            [
+                self::FIRST_MATCH,
+                self::FIRST_MATCH
+            ],
+            [
+                self::SECOND_MATCH,
+                self::SECOND_MATCH
+            ],
+            [
+                self::UNIQUE_ELEMENT
+            ]
+        ];
 
-		$regroupedElements = $this->regroup->regroupIdenticalElements($elements, $comparisonFunction);
+        $regroupedElements = $this->regroup->regroupIdenticalElements($elements, $comparisonFunction);
 
-		$this->assertEquals($expectedSets, $regroupedElements);
-	}
+        $this->assertEquals($expectedSets, $regroupedElements);
+    }
 
-	public function test_ElementMatchingTwoSets_regroupIdenticalElements_ShouldReturnSetRegroupingMatchingSets()
-	{
-		$elements = [];
+    public function test_ElementMatchingTwoSets_regroupIdenticalElements_ShouldReturnSetRegroupingMatchingSets()
+    {
+        $elements = [];
 
-		$firstElement = new \stdClass();
-		$firstElement->firstProperty = self::FIRST_MATCH;
-		$firstElement->secondProperty = 'unique second property';
-		$elements[] = $firstElement;
+        $firstElement = new \stdClass();
+        $firstElement->firstProperty = self::FIRST_MATCH;
+        $firstElement->secondProperty = 'unique second property';
+        $elements[] = $firstElement;
 
-		$secondElement = new \stdClass();
-		$secondElement->firstProperty = 'unique first property';
-		$secondElement->secondProperty = self::SECOND_MATCH;
-		$elements[] = $secondElement;
+        $secondElement = new \stdClass();
+        $secondElement->firstProperty = 'unique first property';
+        $secondElement->secondProperty = self::SECOND_MATCH;
+        $elements[] = $secondElement;
 
-		$thirdElement = new \stdClass();
-		$thirdElement->firstProperty = self::FIRST_MATCH;
-		$thirdElement->secondProperty = self::SECOND_MATCH;
-		$elements[] = $thirdElement;
+        $thirdElement = new \stdClass();
+        $thirdElement->firstProperty = self::FIRST_MATCH;
+        $thirdElement->secondProperty = self::SECOND_MATCH;
+        $elements[] = $thirdElement;
 
-		$comparisonFunction = function($elmt1, $elmt2) {
-			return $elmt1->firstProperty == $elmt2->firstProperty || $elmt1->secondProperty == $elmt2->secondProperty;
-		};
+        $comparisonFunction = function ($elmt1, $elmt2) {
+            return $elmt1->firstProperty == $elmt2->firstProperty || $elmt1->secondProperty == $elmt2->secondProperty;
+        };
 
-		$expectedSets = [
-			[
-				$firstElement,
-				$thirdElement,
-				$secondElement
-			]
-		];
+        $expectedSets = [
+            [
+                $firstElement,
+                $thirdElement,
+                $secondElement
+            ]
+        ];
 
-		$regroupedElements = $this->regroup->regroupIdenticalElements($elements, $comparisonFunction);
+        $regroupedElements = $this->regroup->regroupIdenticalElements($elements, $comparisonFunction);
 
-		$this->assertEquals($expectedSets, $regroupedElements);
-	}
+        $this->assertEquals($expectedSets, $regroupedElements);
+    }
 
-	public function test_NonMatchingElements_regroupByHash_ShouldReturnASetForEachElement()
-	{
-		$elements = [
-			self::FIRST_ELEMENT,
-			self::SECOND_ELEMENT
-		];
+    public function test_NonMatchingElements_regroupByHash_ShouldReturnASetForEachElement()
+    {
+        $elements = [
+            self::FIRST_ELEMENT,
+            self::SECOND_ELEMENT
+        ];
 
-		$hashFonction = function($element)
-		{
-			return md5($element);
-		};
+        $hashFonction = function ($element) {
+            return md5($element);
+        };
 
-		$expectedSets = [
-			[
-				self::FIRST_ELEMENT
-			],
-			[
-				self::SECOND_ELEMENT
-			]
-		];
+        $expectedSets = [
+            [
+                self::FIRST_ELEMENT
+            ],
+            [
+                self::SECOND_ELEMENT
+            ]
+        ];
 
-		$regroupedElements = $this->regroup->regroupByHash($elements, $hashFonction);
+        $regroupedElements = $this->regroup->regroupByHash($elements, $hashFonction);
 
-		$this->assertEquals($expectedSets, $regroupedElements);
-	}
+        $this->assertEquals($expectedSets, $regroupedElements);
+    }
 
-	public function test_MatchingElements_regroupByHash_ShouldReturnSetWithMathingElements() {
-		$elements = [
-			self::FIRST_MATCH,
-			self::SECOND_MATCH,
-			self::UNIQUE_ELEMENT,
-			self::FIRST_MATCH,
-			self::SECOND_MATCH
-		];
+    public function test_MatchingElements_regroupByHash_ShouldReturnSetWithMathingElements()
+    {
+        $elements = [
+            self::FIRST_MATCH,
+            self::SECOND_MATCH,
+            self::UNIQUE_ELEMENT,
+            self::FIRST_MATCH,
+            self::SECOND_MATCH
+        ];
 
-		$hashFonction = function($element)
-		{
-			return md5($element);
-		};
+        $hashFonction = function ($element) {
+            return md5($element);
+        };
 
-		$expectedSets = [
-			[
-				self::FIRST_MATCH,
-				self::FIRST_MATCH
-			],
-			[
-				self::SECOND_MATCH,
-				self::SECOND_MATCH
-			],
-			[
-				self::UNIQUE_ELEMENT
-			]
-		];
+        $expectedSets = [
+            [
+                self::FIRST_MATCH,
+                self::FIRST_MATCH
+            ],
+            [
+                self::SECOND_MATCH,
+                self::SECOND_MATCH
+            ],
+            [
+                self::UNIQUE_ELEMENT
+            ]
+        ];
 
-		$regroupedElements = $this->regroup->regroupByHash($elements, $hashFonction);
+        $regroupedElements = $this->regroup->regroupByHash($elements, $hashFonction);
 
-		$this->assertEquals($expectedSets, $regroupedElements);
-	}
+        $this->assertEquals($expectedSets, $regroupedElements);
+    }
 }
